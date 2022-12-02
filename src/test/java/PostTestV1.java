0e4ed58e3f7f72a9b0cff7e37b1808e44294ac7b
Александр Масляков <lapostov@yandex.ru>
2022-12-02 14:48:35 +0400

    First commit

---
+++ src/test/java/PostTestV1.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostTestV1 {
+    @Test
+    public  void shouldReturnData(){
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some value") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some value"));
+    }
+}
